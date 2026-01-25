0a97578820930f3c374bebcca384dfc4c677e7f6
RyoSugimoto <1042374+RyoSugimoto@users.noreply.github.com>
2026-01-25 14:32:02 +0900

    GitHub Pagesの設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://RyoSugimoto.github.io',
+  base: '/cheatsheet',
   vite: {
     plugins: [tailwindcss()],
   },
